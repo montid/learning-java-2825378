40412f8bca67710c6f45329d57076eaccc965114
montid <dmontingelli@gmail.com>
2022-02-17 14:19:16 +0100

    change main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World from IntelliJ!");
+        int studentAge = 45;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'K';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println("Age\t" + studentAge);
+        System.out.println("First Initial\t" + studentFirstInitial);
+        System.out.println("Last Initial\t" + studentLastInitial);
+        System.out.println("GPA\t" + studentGPA);
+        System.out.println("Perfect Attendance\t" + hasPerfectAttendance);
+
     }
 }
