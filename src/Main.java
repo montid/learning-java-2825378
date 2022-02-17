2bde43cefdc2ab83db11cdd772b27baf57b65f12
montid <dmontingelli@gmail.com>
2022-02-17 15:15:53 +0100

    change main - adding String

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        String studentFirstName = "Dario";
+        String studentLastName = "Montingelli";
         int studentAge = 45;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
         boolean hasPerfectAttendance = true;
 
-        System.out.println("Age\t" + studentAge);
-        System.out.println("First Initial\t" + studentFirstInitial);
-        System.out.println("Last Initial\t" + studentLastInitial);
-        System.out.println("GPA\t" + studentGPA);
-        System.out.println("Perfect Attendance\t" + hasPerfectAttendance);
+
+        System.out.println("Name:\t" + studentFirstName);
+        System.out.println("Surname:\t" + studentLastName);
+        System.out.println("Age:\t" + studentAge);
+        System.out.println("First Initial:\t" + studentFirstInitial);
+        System.out.println("Last Initial:\t" + studentLastInitial);
+        System.out.println("GPA:\t" + studentGPA);
+        System.out.println("Perfect Attendance:\t" + hasPerfectAttendance);
 
     }
 }
