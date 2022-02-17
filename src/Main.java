9091a4551ec71dea1618c606f5acd5ceeb9cebc7
montid <dmontingelli@gmail.com>
2022-02-17 15:48:41 +0100

    change main - adding Scanner input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,14 +12,17 @@ public class Main {
         char studentLastInitial = studentLastName.charAt(0);
         boolean hasPerfectAttendance = true;
 
-
         System.out.println("Name:\t" + studentFirstName);
-        System.out.println("Surname:\t" + studentLastName);
+        System.out.println("Surname:\t" + studentLastName.toUpperCase());
         System.out.println("Age:\t" + studentAge);
         System.out.println("First Initial:\t" + studentFirstInitial);
         System.out.println("Last Initial:\t" + studentLastInitial);
         System.out.println("GPA:\t" + studentGPA);
         System.out.println("Perfect Attendance:\t" + hasPerfectAttendance);
+        System.out.println("What do you want to update the GPA to?");
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
 
+        System.out.println(studentFirstName + " " + studentLastName + "now has a GPA of " + studentGPA);
     }
 }
