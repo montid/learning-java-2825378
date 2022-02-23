d162842e53d2add4f3f6fb830ff007034be17e45
montid <dmontingelli@gmail.com>
2022-02-23 15:25:19 +0100

    MealPrice return usage

--- src/MealPrice.java
+++ src/MealPrice.java
@@ -1,15 +1,22 @@
 public class MealPrice {
 
-    public static void calculateTotalMealPrice (double listedMealPrice, double tipRate , double taxRate) {
+    // public static void calculateTotalMealPrice (double listedMealPrice, double tipRate , double taxRate) {
+    public static double calculateTotalMealPrice (double listedMealPrice, double tipRate , double taxRate) {
+
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
         System.out.println("Your total meail price is: " + result);
+        return result;
     }
 
 
     public static void main(String[] args) {
-        calculateTotalMealPrice(15, 0.20, 0.08);
-        calculateTotalMealPrice(25, 0.18, 0.08);
+        //calculateTotalMealPrice(100, 0.20, 0.08);
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, .08);
+        System.out.println(groupTotalMealPrice);
+
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
     }
 }
